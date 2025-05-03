12151e28b438bc10847c6516dd15d27ec79b2664
Duong Bach Chi <164162779+ImAno177@users.noreply.github.com>
2025-05-03 17:44:51 +0700

    Add files via upload

---
+++ src/components/HeaderAdmin/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="hidden md:block">
+      <desktopIndex />
+    </div>
+
+    <div class="block md:hidden">
+      <mobileIndex />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import desktopIndex from "./DesktopHeader/desktopIndex.vue";
+import mobileIndex from "./MobileHeader/mobileIndex.vue";
+</script>
